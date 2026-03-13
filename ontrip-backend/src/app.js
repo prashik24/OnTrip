c02a263534db37ecffa94a546d44423f52c561a8
Prashik Humane <prashikhumane744@gmail.com>
2026-03-13 11:57:09 +0530

    file update

--- ontrip-backend/src/app.js
+++ ontrip-backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/authRoutes.js";
 import providerRoutes from "./routes/providerRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
