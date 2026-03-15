87c742ac1ebc9ff96456f1e9db9e68c7d90a273b
Prashik Humane <prashikhumane744@gmail.com>
2026-03-15 13:35:37 +0530

    Improve booking routes and API endpoints

--- ontrip-backend/src/routes/bookingRoutes.js
+++ ontrip-backend/src/routes/bookingRoutes.js
@@ -5,6 +5,8 @@ import {
   getMyBookings,
   getProviderBookings,
   updateBookingStatus,
+  getBookingById,
+  downloadInvoice,
 } from "../controllers/bookingController.js";
 import { protect } from "../middleware/auth.js";
 
@@ -15,6 +17,8 @@ router.post("/verify-payment", protect, verifyBookingPayment);
 
 router.get("/mine", protect, getMyBookings);
 router.get("/provider", protect, getProviderBookings);
+router.get("/:id", protect, getBookingById);
+router.get("/:id/invoice", protect, downloadInvoice);
 
 router.put("/:id/status", protect, updateBookingStatus);
 
