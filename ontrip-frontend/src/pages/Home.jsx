fdf70df5f658c2060af4690dfdb1286cf53859b7
Prashik Humane <prashikhumane744@gmail.com>
2026-03-28 20:52:47 +0530

    Update Home page UI and add OnTrip logo asset

--- ontrip-frontend/src/pages/Home.jsx
+++ ontrip-frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import heroImg from "../assets/ontrip-hero.png";
+import logoImg from "../assets/Ontrip.png";
 import "./Home.css";
 
 export default function Home() {
@@ -10,8 +11,17 @@ export default function Home() {
       className="homeFullscreen"
       style={{ backgroundImage: `url(${heroImg})` }}
     >
+      <div className="homeLogoWrap">
+        <img src={logoImg} alt="OnTrip" className="homeLogo" />
+      </div>
+
       {/* Optional overlay content */}
-     
+      {/* 
+      <div className="heroContent">
+        <button onClick={() => navigate("/explore")}>Explore</button>
+        <button onClick={() => navigate("/planner")}>Plan Trip</button>
+      </div>
+      */}
     </section>
   );
 }
