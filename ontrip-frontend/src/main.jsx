67389fc5bdb7ffce9fa6892b652a0ac60954c4ff
Prashik Humane <prashikhumane744@gmail.com>
2026-03-28 18:50:42 +0530

    Update main entry and responsive overrides styling

--- ontrip-frontend/src/main.jsx
+++ ontrip-frontend/src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import "./styles/responsive-overrides.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
