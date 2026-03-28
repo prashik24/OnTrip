07c407b824c2b81ceac25bebedfb086c8b1d8f9d
Prashik Humane <prashikhumane744@gmail.com>
2026-03-28 21:06:42 +0530

    Update Home page, global styles, and remove responsive overrides stylesheet

--- ontrip-frontend/src/main.jsx
+++ ontrip-frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 import "./styles/theme.css";
 import "./styles/global.css";
-import "./styles/responsive-overrides.css";
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- ontrip-frontend/src/pages/Home.jsx
+++ ontrip-frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import heroImg from "../assets/ontrip-hero.png";
-import logoImg from "../assets/OnTrip.png";
+import mobileHero from "../assets/OnTrip.png";
 import "./Home.css";
 
 export default function Home() {
@@ -9,19 +9,19 @@ export default function Home() {
   return (
     <section
       className="homeFullscreen"
-      style={{ backgroundImage: `url(${heroImg})` }}
+      style={{
+        backgroundImage: `url(${heroImg})`,
+      }}
     >
-      <div className="homeLogoWrap">
-        <img src={logoImg} alt="OnTrip" className="homeLogo" />
-      </div>
+      {/* Mobile image layer */}
+      <div
+        className="homeMobileBg"
+        style={{
+          backgroundImage: `url(${mobileHero})`,
+        }}
+      ></div>
 
       {/* Optional overlay content */}
-      {/* 
-      <div className="heroContent">
-        <button onClick={() => navigate("/explore")}>Explore</button>
-        <button onClick={() => navigate("/planner")}>Plan Trip</button>
-      </div>
-      */}
     </section>
   );
 }
