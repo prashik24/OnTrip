878fa6b2a730fbe6313c11ef34cd7312048031f4
Prashik Humane <prashikhumane744@gmail.com>
2026-03-28 21:22:45 +0530

    Update Home page styling and component

--- ontrip-frontend/src/pages/Home.jsx
+++ ontrip-frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import heroImg from "../assets/ontrip-hero.png";
-import mobileHero from "../assets/OnTrip.png";
 import "./Home.css";
 
 export default function Home() {
@@ -11,16 +10,8 @@ export default function Home() {
       className="homeFullscreen"
       style={{ backgroundImage: `url(${heroImg})` }}
     >
-      {/* Mobile image layer */}
-      <div
-        className="homeMobileBg"
-        style={{ backgroundImage: `url(${mobileHero})` }}
-      ></div>
-
       {/* Optional overlay content */}
-      <div className="heroContent">
-        {/* buttons / content if needed */}
-      </div>
+     
     </section>
   );
 }
