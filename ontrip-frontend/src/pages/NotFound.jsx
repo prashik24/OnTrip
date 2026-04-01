20adbb769d13d0ba7fe7458a8878867ae4a70844
Prashik Humane <prashikhumane744@gmail.com>
2026-04-01 19:07:07 +0530

    Enhance NotFound page component

--- ontrip-frontend/src/pages/NotFound.jsx
+++ ontrip-frontend/src/pages/NotFound.jsx
@@ -3,12 +3,32 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="container nf">
-      <div className="card nfCard">
-        <div className="nfTitle">404</div>
-        <div className="nfSub">Page not found.</div>
-        <Link className="btn btnPrimary" to="/">Go Home</Link>
+    <section className="nfWrap">
+      <div className="nfGlow nfGlowOne" />
+      <div className="nfGlow nfGlowTwo" />
+
+      <div className="container nf">
+        <div className="nfCard">
+          <div className="nfCode">404</div>
+          <div className="nfBadge">Oops! Page Missing</div>
+
+          <h1 className="nfTitle">This page could not be found</h1>
+
+          <p className="nfSub">
+            The page you are looking for may have been removed, renamed, or the
+            link might be broken.
+          </p>
+
+          <div className="nfActions">
+            <Link className="nfBtn nfBtnPrimary" to="/">
+              Go Home
+            </Link>
+            <Link className="nfBtn nfBtnGhost" to="/explore">
+              Explore Places
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
