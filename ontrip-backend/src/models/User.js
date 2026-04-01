2bedd3f04bff85e57844a101c7a0d211af01f636
Prashik Humane <prashikhumane744@gmail.com>
2026-04-02 01:41:38 +0530

    Update User model for social integration

--- ontrip-backend/src/models/User.js
+++ ontrip-backend/src/models/User.js
@@ -60,6 +60,10 @@ const userSchema = new mongoose.Schema(
       default: "user",
     },
 
+    socialProfileCreated: {
+      type: Boolean,
+      default: false,
+    },
     isOnline: {
       type: Boolean,
       default: false,
