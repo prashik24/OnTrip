75fb35c855460af7c0835d3b5570dcc60c880257
Prashik Humane <prashikhumane744@gmail.com>
2026-04-06 10:33:29 +0530

    Update subscriber routes

--- ontrip-backend/src/routes/subscriberRoutes.js
+++ ontrip-backend/src/routes/subscriberRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { subscribeEmail } from "../controllers/subscriberController.js";
+import { protect } from "../middleware/auth.js";
+import {
+  getSubscriberStatus,
+  subscribeUser,
+  unsubscribeUser,
+} from "../controllers/subscriberController.js";
 
 const router = express.Router();
 
-router.post("/subscribe", subscribeEmail);
+router.get("/status", protect, getSubscriberStatus);
+router.post("/subscribe", protect, subscribeUser);
+router.post("/unsubscribe", protect, unsubscribeUser);
 
 export default router;
