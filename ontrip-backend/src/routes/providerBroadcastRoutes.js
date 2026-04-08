c6f819d7b04ec6f366ba574ee7adefd38ab5c706
Prashik Humane <prashikhumane744@gmail.com>
2026-04-09 00:26:04 +0530

    feat: update provider broadcast routes for subscriber broadcast APIs

--- ontrip-backend/src/routes/providerBroadcastRoutes.js
+++ ontrip-backend/src/routes/providerBroadcastRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   sendProviderBroadcast,
   getProviderBroadcasts,
+  getAllProviderBroadcasts,
 } from "../controllers/providerBroadcastController.js";
 import { protect } from "../middleware/auth.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/send", protect, sendProviderBroadcast);
 router.get("/my", protect, getProviderBroadcasts);
+router.get("/all", protect, getAllProviderBroadcasts);
 
 export default router;
